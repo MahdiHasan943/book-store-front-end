4ebc0018672e77f773f8f662d7c14274ece90b32
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-10-04 20:24:37 +0600

    added footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,47 @@ module.exports = {
     ]
   },
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'exsm':'353px',
+        'gm': '541px',
+        'pm':'430px',
+        'nm':'895px',
+        'sm': '640px',
+        'six':'700px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1025px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+         
+        '2xl': '1536px',
+        '3xl': '1736px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+        // volkhov: ['Volkhov', 'serif'],
+        popping: ['Poppins', 'sans-serif'],
+        sofia: ['Sofia Sans', 'sans-serif'],
+        Arimo: [ 'Arimo', 'sans-serif']
+
+        
+      
+
+      },
+  
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        'ButtonShadow': ' 0px 18px 40px 0px rgba(255, 110, 107, 0.30)',
+        'summery': '2px 2px 4px 0px rgba(0, 0, 0, 0.05), -2px -2px 4px 0px rgba(0, 0, 0, 0.05)'
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
